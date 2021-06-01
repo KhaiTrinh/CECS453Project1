c58a899702719fec826f07beffff83180b07d2e0
Khai Trinh <khaitrinh99@gmail.com>
2021-06-01 14:29:08 -0700

    Update LoginActivity.java

--- app/src/main/java/com/example/cecs453project1/LoginActivity.java
+++ app/src/main/java/com/example/cecs453project1/LoginActivity.java
@@ -25,7 +25,7 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
             }
         });
 
