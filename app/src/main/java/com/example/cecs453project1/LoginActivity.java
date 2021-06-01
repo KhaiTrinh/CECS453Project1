2257ccd84e7400200e94fbd9b5823c9f1a95c6a8
Khai Trinh <khaitrinh99@gmail.com>
2021-06-01 14:16:47 -0700

    Added some functions for login.

--- app/src/main/java/com/example/cecs453project1/LoginActivity.java
+++ app/src/main/java/com/example/cecs453project1/LoginActivity.java
@@ -2,13 +2,39 @@ package com.example.cecs453project1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText txtLoginUsername, txtLoginPassword;
+    private Button btnLogin, btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        txtLoginUsername = findViewById(R.id.txtLoginUsername);
+        txtLoginPassword = findViewById(R.id.txtLoginPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnSignup);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signup_page = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(signup_page);
+            }
+        });
     }
 }
